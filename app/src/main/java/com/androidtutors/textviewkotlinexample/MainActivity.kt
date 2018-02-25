ef06099b7f0bcd12e0b8a1d81008985dce72dede
Megha <compchap.megha@gmail.com>
2018-02-25 12:19:38 -0800

    Initial Code

---
+++ app/src/main/java/com/androidtutors/textviewkotlinexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.androidtutors.textviewkotlinexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //TODO 6 : Change the text of textview_hello to "Learning Kotlin"
+
+        //TODO 7 : Adding onClickListener on textview_hello
+        //TODO 8 : Changing the text of textview_hello to "You clicked me!"
+
+    }
+}
