89473df33a5a4d846f8a0b456c6661081bac8340
Megha <compchap.megha@gmail.com>
2018-02-25 17:45:04 -0800

    TODO 6 : Change the text of textview_hello to "Learning Kotlin"

--- app/src/main/java/com/androidtutors/textviewkotlinexample/MainActivity.kt
+++ app/src/main/java/com/androidtutors/textviewkotlinexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.androidtutors.textviewkotlinexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //TODO 6 : Change the text of textview_hello to "Learning Kotlin"
+        textview_hello.text = "Learnig Kotlin"
 
         //TODO 7 : Adding onClickListener on textview_hello
         //TODO 8 : Changing the text of textview_hello to "You clicked me!"
